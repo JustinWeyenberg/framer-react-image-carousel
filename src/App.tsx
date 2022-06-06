9580ccd8e059d7404378115d5496bd9758811f59
Justin Weyenberg <48866086+JustinWeyenberg@users.noreply.github.com>
2022-06-06 10:36:53 -0500

    Using Fetch to get list of images Using fetched list of images so we can have static URLs for the demo.

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,38 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import ImageCarousel from "./ImageCarousel/ImageCarousel";
 import "./styles.css";
 
+interface PicsumImage {
+  url: string;
+  author: string;
+}
+
 export default function App() {
-  const [images] = useState(
-    Array.from(Array(20).keys()).map((id) => ({
-      altText: "Random Image",
-      url: `https://picsum.photos/1000/500?random=${id}`
-    }))
-  );
+  const [images, setImages] = useState([]);
+
+  const getImages = async () => {
+    const randomImages = await fetch("https://picsum.photos/v2/list?limit=20");
+    const resultImages: Array<PicsumImage> = await randomImages.json();
+
+    console.log(resultImages);
+    return resultImages;
+  };
+
+  useEffect(() => {
+    const getAndSetImages = async () => {
+      if (images.length === 0) {
+        const resultImages = await getImages();
+        setImages(
+          resultImages.map((image) => ({
+            url: `https://picsum.photos/id/${image.id}/1000/500`,
+            altText: `Photo by ${image.author}`
+          }))
+        );
+      }
+    };
+
+    getAndSetImages();
+  }, [images]);
 
   return (
     <div className="App">
